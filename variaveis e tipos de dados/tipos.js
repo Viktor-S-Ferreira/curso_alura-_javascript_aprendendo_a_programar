925bb20d7d3a1e5533278f502e9d6a9c029f2ff7
Viktor Ferreira <viktorvr0@gmail.com>
2026-02-04 21:04:40 -0300

    identificando tipos de variaveis

---
+++ variaveis e tipos de dados/tipos.js
@@ -0,0 +1,24 @@
+let nome = 'Viktor'
+let idade = 27
+let altura = 1.80
+let estaChovendo = false
+let cidade
+let aprendeuJs = null
+let simbolo = Symbol()
+
+console.log(typeof nome)
+console.log(typeof idade)
+console.log(typeof altura)
+console.log(typeof estaChovendo)
+console.log(typeof cidade)
+console.log(aprendeuJs)
+console.log(simbolo)
+
+// possiveis tipos de dados do JS:
+// string
+// number
+// number
+// boolean
+// undefined
+// null
+// Symbol()
